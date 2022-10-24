c08ad00b3cd6ca094e068c05504f01233112d93b
Adriana Morales <amorales25@gmail.com>
2022-10-24 13:58:34 -0400

    installing prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,27 @@
-const path = require("path");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: "development",
-    context: path.join(__dirname, "app"),
-    entry: ["./js/app.js"],
+    mode: 'development',
+    context: path.join(__dirname, 'app'),
+    entry: ['./js/app.ts'],
     output: {
-        path: path.join(__dirname, "public"),
-        filename: "bundle.js",
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
     },
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ["babel-loader"],
-            },
-        ],
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'app/index.html'),
+            inject: 'body'
+        })
+    ]
 };
